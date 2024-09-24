171cbde622cbb4ca3fcc9aee27e29d8a3ff09dfc
geloportugal <portugal.angelo2020@gmail.com>
2024-09-24 09:19:04 +0800

    first file or first upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+#ignore sample
+
+print("Hello World")
+
+firstname = "Angelo"
+lastname = "Portugal"
+print(firstname + lastname)
+print(firstname + " " + lastname)
+print(firstname , lastname)
---
+++ laboratory2.py
@@ -0,0 +1,10 @@
+firstname = "Angelo"
+lastname = "Portugal"
+age = str(18)
+fullname = firstname + " " + lastname
+print("Hello, my name is" , fullname + "." , "I am" , age , "years old.")
+if (fullname.upper == True):
+    print(fullname)
+else:
+    new_full = fullname.upper()
+    print(new_full)
